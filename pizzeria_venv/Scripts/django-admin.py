f56cf1635923fc735330846cf356aa124c1f4dcc
Abbey <abbey_sellers1@baylor.edu>
2022-12-11 03:23:50 -0600

    initial

---
+++ pizzeria_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\abbey\onedrive\fall 2022\advpython4322\django_pizzeria\pizzeria_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
